f37114d716e5a5779c56b4f13b3fb70a88555f17
jtr1k <jtr1k@yandex.ru>
2026-05-10 02:54:36 +0300

    Add adapter implementation

--- src/python_proxychains/adapter.py
+++ src/python_proxychains/adapter.py
@@ -1,17 +1,34 @@
 from requests import PreparedRequest, Response
 from requests.adapters import HTTPAdapter
+from requests.exceptions import ConnectionError
 
+from .config_models import ProxyProtoEnum
 from .router import Router
 
 
 class RoutingProxyAdapter(HTTPAdapter):
-
-    def __init__(self, router: Router):
+    def __init__(self, router: Router, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.router = router
 
     def send(self, request: PreparedRequest, **kwargs) -> Response:
+        proxy = self.router.get_proxy(request.url)
 
-        proxy = router.get_proxy(request.url).url
+        if proxy.protocol == ProxyProtoEnum.blackhole:
+            raise ConnectionError(f"Request blocked by blackhole route: {request.url}")
 
-        kwargs["proxies"] = {"https://": proxy, "http://": proxy}
+        if proxy.protocol == ProxyProtoEnum.freedom:
+            kwargs.pop("proxies", None)
+            return super().send(request, **kwargs)
+
+        if proxy.url is None:
+            raise ValueError(f"Proxy {proxy.tag} has no URL")
+
+        proxy_url = str(proxy.url)
+
+        kwargs["proxies"] = {
+            "http": proxy_url,
+            "https": proxy_url,
+        }
+
         return super().send(request, **kwargs)
